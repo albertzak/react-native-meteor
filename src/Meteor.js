d42cc3836264af28e353112e18759295aa125e27
Albert Zak <me@albertzak.com>
2019-09-11 20:14:57 +0200

    Update NetInfo

--- src/Meteor.js
+++ src/Meteor.js
@@ -18,7 +18,7 @@ import Accounts from './user/Accounts';
 
 let NetInfo;
 if (isReactNative) {
-    NetInfo = require('react-native').NetInfo;  // eslint-disable-line
+    NetInfo = require('@react-native-community/netinfo');  // eslint-disable-line
 }
 
 
